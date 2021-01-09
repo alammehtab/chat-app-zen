706874482c3211d025da6c931038aa325ec3e8e8
Mehtab Alam <mehtaba728@mail.com>
2021-01-09 14:50:27 +0500

    redux and react are now ready to be connected

---
+++ src/containers/Home/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Chat Application</h1>
+        <p>Welcome To My Chat Application</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,6 +2,6 @@ const INITIAL_STATE = {
   users: [],
 };
 
-const reducer = (state = INITIAL_STATE) => state;
+const reducer = (state = INITIAL_STATE, action) => state;
 
 export default reducer;
